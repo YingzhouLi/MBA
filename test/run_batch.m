ed38f24128942cb613316d67fbdfb578b8570821
lyzh588 <lyzh588@gmail.com>
2014-11-09 01:31:47 -0800

    Add amplitude functions

--- test/run_batch.m
+++ test/run_batch.m
@@ -4,13 +4,13 @@ if(~exist(log_path, 'dir'))
     mkdir(log_path);
 end
 
-func_list = {'fun2'};
+func_list = {'fun1'};
 
 for func_i = 1:length(func_list)
     func_name = func_list{func_i};
     for N = 2.^(8:11)
-        fid = fopen([log_path 'HBA_' func_name '_' num2str(N) '.log'],'a+');
-        for EPS = [2:2:10]
+        fid = fopen([log_path 'HBA_' func_name '_amp_' num2str(N) '.log'],'a+');
+        for EPS = [5:2:11]
             runCbs(N, func_name, EPS, fid);
             fprintf('Func %s, N %4d, EPS %2d finished.\n',func_list{func_i},N,EPS);
         end
